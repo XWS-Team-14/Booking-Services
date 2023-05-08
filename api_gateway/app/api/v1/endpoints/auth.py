9cdbd9474b6b2ff08528fbc30997f4ff53b74cee
MiskLG <aca.hornjak@gmail.com>
2023-05-09 01:29:47 +0200

    chore: change django gateway to fastapi
    It was commented before that it will be better to use fastapi but it was
    never changed even tho we had code completed. This fixes that issue

---
+++ api_gateway/app/api/v1/endpoints/auth.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, status
+from fastapi_utils.cbv import cbv
+from loguru import logger
+
+from app import schemas
+
+router = APIRouter()
+
+
+@cbv(router)
+class Accommodation:
+    # Useful for shared dependencies
+    @router.delete(
+        "/test",
+        status_code=status.HTTP_204_NO_CONTENT,
+        description="Test fastapi",
+    )
+    async def test(self, payload: schemas.AccommodationDelete) -> None:
+        logger.info(f"App tested and got number {payload.id}")
+
+
+@router.delete(
+    "/test2",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Test fastapi",
+)
+async def test(self, payload: schemas.AccommodationDelete) -> None:
+    logger.info(f"App tested and got number {payload.id}")
