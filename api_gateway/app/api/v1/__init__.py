0a5494d087c49cc3b10317252ef941a1333d2966
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-05-09 23:10:40 +0200

    chore: restructure api folder

--- api_gateway/app/api/v1/__init__.py
+++ api_gateway/app/api/v1/__init__.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth
-from app.config import get_yaml_config
+from .auth import router as auth_router
+from .accommodation import router as accommodation_router
 
-config = get_yaml_config().get("app")
-router = APIRouter(prefix=f"/{config.get('api_prefix')}")
-
-# Include your endpoints here
-router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+# include version of the api here
+router = APIRouter(prefix="/api")
+router.include_router(auth_router, prefix="/auth")
+router.include_router(accommodation_router, prefix="/accommodation")
