ea7de0df90e1d16a8c30533cf0160b526a427231
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2023-05-12 09:28:15 +0200

    feat: create method via api gateway

--- api_gateway/app/api/v1/availability.py
+++ api_gateway/app/api/v1/availability.py
@@ -1,3 +1,4 @@
+from app.schemas.availability import AvailabilityDto
 from app.config import get_yaml_config
 from fastapi import APIRouter, status
 from fastapi.responses import Response
@@ -6,7 +7,7 @@ from google.protobuf import json_format
 import json
 from loguru import logger
 import grpc
-from proto import availability_crud_pb2_grpc
+from proto import availability_crud_pb2_grpc,availability_crud_pb2
 
 
 router = APIRouter(
@@ -30,7 +31,45 @@ async def getAll():
         stub = availability_crud_pb2_grpc.AvailabilityCrudStub(channel)
         logger.info("Gateway processing getAll Availability data");
         data = await stub.GetAll({});
-        json = json_format.MessageToJson(data)
+        json = json_format.MessageToJson(data,preserving_proto_field_name=True)
     return Response(
         status_code=200, media_type="application/json", content=json
+    )
+    
+@router.post(
+    "/create",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Create new availability",
+)
+async def create(item:AvailabilityDto):
+    logger.info("Gateway processing create Availability request");
+    
+    availability_server = (
+        get_yaml_config().get("availability_server").get("ip")
+        + ":"
+        + get_yaml_config().get("availability_server").get("port")
+    )
+    async with grpc.aio.insecure_channel(availability_server) as channel:
+        stub = availability_crud_pb2_grpc.AvailabilityCrudStub(channel)
+        
+        availability = availability_crud_pb2.AvailabilityDto()
+        availability.availability_id = item.availability_id
+        availability.accomodation_id = item.accomodation_id
+        availability.interval.date_start = item.interval.date_start
+        availability.interval.date_end = item.interval.date_end
+        availability.base_price = item.base_price
+        availability.pricing_type = item.pricing_type
+        for pricing in item.special_pricing:
+            availability.special_pricing.append(availability_crud_pb2.SpecialPricing(
+                title = pricing.title,
+                pricing_markup = pricing.pricing_markup
+            ))
+            
+        response = await stub.Create(availability);
+        if response.status == "Invalid date":
+            return Response(
+                status_code=200, media_type="application/json", content="Invalid date"
+            )
+    return Response(
+        status_code=200, media_type="application/json", content="Success"
     )
