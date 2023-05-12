71aa7d8eabd290e0adbebd8a515d746f841b6177
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2023-05-12 11:22:30 +0200

    feat: update and delete methods api gateway

--- api_gateway/app/api/v1/availability.py
+++ api_gateway/app/api/v1/availability.py
@@ -95,3 +95,53 @@ async def create(item:AvailabilityDto):
     return Response(
         status_code=200, media_type="application/json", content="Success"
     )
+@router.put(
+    "/update",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Update availability",
+)
+async def update(item:AvailabilityDto):
+    logger.info("Gateway processing update Availability request");
+    availability_server = (
+        get_yaml_config().get("availability_server").get("ip")
+        + ":"
+        + get_yaml_config().get("availability_server").get("port")
+    )
+    async with grpc.aio.insecure_channel(availability_server) as channel:
+        stub = availability_crud_pb2_grpc.AvailabilityCrudStub(channel)
+        
+        availability = availability_crud_pb2.AvailabilityDto()
+        availability.availability_id = item.availability_id
+        availability.accomodation_id = item.accomodation_id
+        availability.interval.date_start = item.interval.date_start
+        availability.interval.date_end = item.interval.date_end
+        availability.base_price = item.base_price
+        availability.pricing_type = item.pricing_type
+        for pricing in item.special_pricing:
+            availability.special_pricing.append(availability_crud_pb2.SpecialPricing(
+                title = pricing.title,
+                pricing_markup = pricing.pricing_markup
+            ))
+            
+        response = await stub.Update(availability);
+    return Response(
+        status_code=200, media_type="application/json", content=response.status
+    )
+@router.delete(  
+    "/delete/{item_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Update availability",
+) 
+async def delete(item_id):
+    logger.info("Gateway processing delete Availability request");
+    availability_server = (
+        get_yaml_config().get("availability_server").get("ip")
+        + ":"
+        + get_yaml_config().get("availability_server").get("port")
+    )
+    async with grpc.aio.insecure_channel(availability_server) as channel:   
+        stub = availability_crud_pb2_grpc.AvailabilityCrudStub(channel)
+        data = await stub.Delete(availability_crud_pb2.AvailabilityId(id=item_id));
+    return Response(
+        status_code=200, media_type="application/json", content=data.status
+    )
