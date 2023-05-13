56e7bb6ad2307c1b0e0f4f0c4954d53bf90e953f
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-05-13 02:00:55 +0200

    feat: implement delete user

--- api_gateway/app/api/v1/user.py
+++ api_gateway/app/api/v1/user.py
@@ -23,7 +23,8 @@ class User:
         status_code=status.HTTP_200_OK,
         description="Update user details",
     )
-    async def update_user_details(self, payload: schemas.UserDetailsUpdate, access_token: Annotated[str | None, Cookie()] = None) -> Response:
+    async def update_user_details(self, payload: schemas.UserDetailsUpdate,
+                                  access_token: Annotated[str | None, Cookie()] = None) -> Response:
         user_id = get_id_from_access_token(access_token)
         logger.info(f"Tested update user details {user_id}")
         user_server = get_server("user_server")
@@ -33,15 +34,27 @@ class User:
                 id=str(user_id), first_name=payload.first_name, last_name=payload.last_name,
                 home_address=payload.home_address, gender=payload.gender))
             if grpc_response.error_message:
-                return Response(status_code=grpc_response.error_code, media_type="text/html", content=grpc_response.error_message)
+                return Response(status_code=grpc_response.error_code, media_type="text/html",
+                                content=grpc_response.error_message)
         return Response(
             status_code=200, media_type="text/html", content="User updated."
         )
 
     @router.delete(
-        "/{user_id}",
+        "/",
         status_code=status.HTTP_200_OK,
         description="Delete user",
     )
-    async def login(self, payload: schemas.Login) -> None:
-        logger.info(f"Tested delete user {payload.email}")
+    async def delete(self, access_token: Annotated[str | None, Cookie()] = None) -> Response:
+        user_id = get_id_from_access_token(access_token)
+        logger.info(f"Tested delete user {user_id}")
+        user_server = get_server("user_server")
+        async with grpc.aio.insecure_channel(user_server) as channel:
+            stub = user_pb2_grpc.UserServiceStub(channel)
+            grpc_response = await stub.Delete(user_pb2.UserId(id=str(user_id)))
+            if grpc_response.error_message:
+                return Response(status_code=grpc_response.error_code, media_type="text/html",
+                                content=grpc_response.error_message)
+        return Response(
+            status_code=200, media_type="text/html", content="User deleted."
+        )
