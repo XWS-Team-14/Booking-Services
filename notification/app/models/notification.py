5c56b4add9dcb4cc47b2040265505094a9751080
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-06-01 12:41:09 +0200

    feat: create proto files

--- notification/app/models/notification.py
+++ notification/app/models/notification.py
@@ -1,17 +1,13 @@
-import uuid
-
-from app.models.notification_status import NotificationStatus
-from app.models.notification_type import NotificationType
 from app.models.receiver import Receiver
 from app.models.sender import Sender
 
 
 class Notification:
     key: str
-    type: NotificationType
+    type: str
     title: str
     content: str
     sender: Sender
     receiver: Receiver
-    status: NotificationStatus
+    status: str
     timestamp: str
