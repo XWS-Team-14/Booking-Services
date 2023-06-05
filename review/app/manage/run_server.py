70c34fd9c2e152953460e3c24d856d8da3d30f61
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-06-05 21:38:45 +0200

    feature: set up review microservice

---
+++ review/app/manage/run_server.py
@@ -0,0 +1,22 @@
+from loguru import logger
+import grpc
+_cleanup_coroutines = []
+
+
+async def serve(port):
+    server = grpc.aio.server()
+    # Add services
+    server.add_insecure_port('[::]:'+port)
+    logger.info('Starting GRPC server')
+    await server.start()
+    logger.success(f'GRPC server has started on port {port}, waiting for termination')
+
+    async def server_graceful_shutdown(*_):
+        logger.info("Starting graceful shutdown...")
+        # Shuts down the server with 5 seconds of grace period. During the
+        # grace period, the server won't accept new connections and allow
+        # existing RPCs to continue within the grace period.
+        await server.stop(5)
+
+    _cleanup_coroutines.append(server_graceful_shutdown())
+    await server.wait_for_termination()
